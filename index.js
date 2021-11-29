44bed79b779cdd609be13903c517626669410e2e
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-11-29 11:55:50 +0700

    add post request function

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const dataProker = require("./models/proker");
+const cors = require("cors");
 
 const app = express();
 
 require("./config/connection");
 
-app.get("/", (req, res) => {
-	res.send("success");
+app.use(cors());
+app.use(express.json());
+
+app.get("/proker", async (req, res) => {
+	const proker = await dataProker.find();
+	res.send(proker);
+});
+
+app.post("/proker", (req, res) => {
+	dataProker.insertMany(req.body, res.send("data has been successfully added"));
 });
 
 app.listen(5000, console.log("app is run at http://localhost:5000"));
