276d87a9feef7f52a59c182efa7bbeb318c02f58
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-12-01 14:10:41 +0700

    add function to update prokers status

--- index.js
+++ index.js
@@ -18,4 +18,21 @@ app.post("/proker", (req, res) => {
 	dataProker.insertMany(req.body, res.send("data has been successfully added"));
 });
 
+app.get("/proker/:id", async (req, res) => {
+	const data = await dataProker.findById(req.params.id);
+	res.send(data);
+});
+
+app.delete("/proker/:id", async (req, res) => {
+	const data = await dataProker.deleteOne({ _id: req.params.id });
+	res.json(data);
+});
+
+app.put("/proker/:id", async (req, res) => {
+	await dataProker.findByIdAndUpdate(req.params.id, {
+		$set: req.body,
+	});
+	res.send("data has been successfully edited");
+});
+
 app.listen(5000, console.log("app is run at http://localhost:5000"));
